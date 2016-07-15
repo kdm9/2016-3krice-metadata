fe52fd6be142affa873641493c71e3d38b529b6a
Kevin Murray <spam@kdmurray.id.au>
2016-07-15 10:40:27 +1000

    Add supergroup into run jsons

--- set2json.py
+++ set2json.py
@@ -5,18 +5,27 @@ import json
 from os import path
 import sys
 
-
+supergroups = {
+    'indica':               'Indica',
+    'japonica':             'Japonica',
+    'temperate japonica':   'Japonica',
+    'tropical japonica':    'Japonica',
+}
 md = metadata.load_csv("all_25228runs_raw.csv")
 samples = {}
+groups = {}
 for run in md:
     samples[run['sra_id']] = run['sra_sample']
+    grp = supergroups.get(run['group'].lower(), 'other')
+    groups[run['sra_id']] = grp
 
 for setfile in sys.argv[1:]:
     jsonfn = setfile + '.json'
     with open(setfile) as fh, open(jsonfn, 'w') as jsonfh:
-        js = defaultdict(list)
+        js = defaultdict(lambda: defaultdict(list))
         for run in fh:
             run = run.strip()
             sample = samples[run]
-            js[sample].append(run)
+            group = groups[run]
+            js[group][sample].append(run)
         json.dump(js, jsonfh, indent=1)
